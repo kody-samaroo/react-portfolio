902af18f3f068845305f454deec7aad500c69cfe
Kody Samaroo <github email address>
2021-06-06 18:16:41 -0400

    added bio component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Bio from "./Bio"
+import Projects from "./Projects"
+
+function App() {
+  
+  return (
+    <div className="app">
+          <Bio/>
+          <Projects/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bio.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Bio() {
+
+    return (
+        <div className="bio">
+            <p className="name"> Kody Samaroo </p>
+            <p> <a href="https://github.com/jodycola">Github |</a> 
+            <a href="https://www.linkedin.com/in/kody-samaroo/"> LinkedIn |</a>
+            <a href="https://kodysamaroo.medium.com/"> Blog</a> </p>
+            
+            <p className="job-title"> Software Engineer </p>
+            <p className="about"> Full Stack Software Engineer experienced in Ruby on Rails, JavaScript and React frameworks with a background study in  Information Technology, Data Analytics and Data Management. As a natural leader and calculated decision maker I have structured databases in order to resourcefully manage IT systems. I am passionate to use new technologies to find simple solutions to analytically make resourceful decisions for complex social problems. </p>
+        </div>
+    )
+}
+
+export default Bio;
